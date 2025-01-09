bd6ce9db7cd430bb66258372ff184f1e1003162a
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-09 14:59:42 +0600

    SC-11 Generate subscriptions CRUD frame

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
