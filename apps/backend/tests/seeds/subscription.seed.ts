631f5c9fe9243b6f4a91fadfbb3ceef37c9f240a
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-13 22:37:06 +0600

    SC-14 Fix seeds

--- apps/backend/tests/seeds/subscription.seed.ts
+++ apps/backend/tests/seeds/subscription.seed.ts
@@ -1,9 +1,30 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import {
+  PrismaClient,
+  Prisma,
+  SubscriptionPeriod,
+  Currency,
+} from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import { seedUser } from './user.seed';
 
 const prisma = new PrismaClient();
 
-export async function seedSubscription(data: Prisma.SubscriptionCreateInput) {
-  return await prisma.user.create({
-    data,
+export async function seedSubscription(
+  data: Partial<Prisma.SubscriptionCreateInput>
+) {
+  const defaultData: Prisma.SubscriptionCreateInput = {
+    name: faker.lorem.sentence(),
+    period: SubscriptionPeriod.MONTHLY,
+    centsPerPeriod: faker.number.int(10000),
+    currency: Currency.USD,
+    startedAt: faker.date.past(),
+    user: data.user
+      ? data.user
+      : ((await seedUser()) as Prisma.UserCreateNestedOneWithoutSubscriptionsInput),
+    ...data,
+  };
+
+  return await prisma.subscription.create({
+    data: defaultData,
   });
 }
--- apps/backend/tests/seeds/user.seed.ts
+++ apps/backend/tests/seeds/user.seed.ts
@@ -1,22 +1,16 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import { faker } from '@faker-js/faker';
-import { AuthService } from '../../src/modules/auth/auth.service';
 
 const prisma = new PrismaClient();
 
-export async function seedUser(
-  authService: AuthService,
-  data: Partial<Prisma.UserCreateInput> = {}
-) {
+export async function seedUser(data: Partial<Prisma.UserCreateInput> = {}) {
   const defaultData: Prisma.UserCreateInput = {
     email: faker.internet.email(),
     subscriptions: data.subscriptions || {
       create: [],
     },
+    password: faker.internet.password(),
     ...data,
-    password: await authService.hashPassword(
-      data.password ? data.password : faker.internet.password()
-    ),
   };
 
   return await prisma.user.create({
