f5710ece99f891f79484ca72e17e22c53ff7a21d
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-20 16:30:57 +0600

    SC-18 Add react router, deleted unused fileds, updated readme.md, add general linting rule

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import './App.css';
 import { Button } from 'antd';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router';
+import { ROUTES } from './router/routes';
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Button>test</Button>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={ROUTES.HOME}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={ROUTES.SIGN_UP}>Sign Up</NavLink>
+            </li>
+            <li>
+              <NavLink to={ROUTES.LOGIN}>Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path={ROUTES.HOME}></Route>
+          <Route path={ROUTES.SIGN_UP}></Route>
+          <Route path={ROUTES.LOGIN}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
