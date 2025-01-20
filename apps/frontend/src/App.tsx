4a00046f74fe91ed56410f4c99ad90fee60acec4
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-20 17:21:07 +0600

    Add styled components and layout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { Button } from 'antd';
-import { BrowserRouter, NavLink } from 'react-router';
-import { ROUTES } from './router/routes';
+import { BrowserRouter } from 'react-router';
 import { AppRouter } from './router/AppRouter';
+import AppLayout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      {/*TODO: Move to navigation component*/}
-      <div>
-        <Button>test</Button>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to={ROUTES.HOME}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to={ROUTES.SIGN_UP}>Sign Up</NavLink>
-            </li>
-            <li>
-              <NavLink to={ROUTES.LOGIN}>Login</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <AppRouter />
+      <AppLayout>
+        <AppRouter />
+      </AppLayout>
     </BrowserRouter>
   );
 }
