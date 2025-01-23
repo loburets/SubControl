db8f5c55ff7d6841c3fa285be7d0a090a1d97e59
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-23 17:45:52 +0600

    SC-39 Fix ant design compatibility with react 19 warning

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { setupAxiosInterceptors } from './utils/axiosInterceptors';
+import '@ant-design/v5-patch-for-react-19';
 
 setupAxiosInterceptors();
 
