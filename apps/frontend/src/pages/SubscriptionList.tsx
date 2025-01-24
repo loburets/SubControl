87c270a25510cc90f166e81b574c5d511f0e3b57
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-24 18:59:54 +0600

    Add search

--- apps/frontend/src/pages/SubscriptionList.tsx
+++ apps/frontend/src/pages/SubscriptionList.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react';
-import { Alert, Row, Col, Button, Space } from 'antd';
+import React, { useMemo, useState } from 'react';
+import { Alert, Row, Col, Button } from 'antd';
 import { MainContentWrapper } from '../components/Layout/MainContentWrapper';
 import { Title } from '../components/UI/Title';
 import { useSubscriptionList } from '../queries/subscriptions.query';
@@ -10,22 +10,31 @@ import { SubscriptionSkeleton } from '../components/UI/SubscriptionSkeleton';
 import { sortSubscriptionsByNextPayment } from '../utils/subscriptionsHelper';
 import { Hider } from '../components/UI/Hider';
 import { SidesSplitter } from '../components/UI/SidesSplitter';
+import { Search } from '../components/UI/Search';
 
 const SubscriptionList: React.FC = () => {
   const { isLoading, error, data } = useSubscriptionList();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredSubscriptions = useMemo(() => {
+    const lowerCaseQuery = searchQuery.toLowerCase().trim();
+    return (data?.subscriptions || []).filter((s) =>
+      s.name.toLowerCase().includes(lowerCaseQuery)
+    );
+  }, [data, searchQuery]);
   const activeSubscriptions = useMemo(
     () =>
-      (data?.subscriptions || [])
+      filteredSubscriptions
         .filter((s) => !s.cancelledAt)
         .sort(sortSubscriptionsByNextPayment),
-    [data]
+    [filteredSubscriptions]
   );
   const cancelledSubscriptions = useMemo(
     () =>
-      (data?.subscriptions || [])
+      filteredSubscriptions
         .filter((s) => s.cancelledAt)
         .sort(sortSubscriptionsByNextPayment),
-    [data]
+    [filteredSubscriptions]
   );
 
   if (error) {
@@ -44,20 +53,23 @@ const SubscriptionList: React.FC = () => {
     <MainContentWrapper>
       <Title level={1}>Your subscriptions</Title>
 
-      <Hider desktopOnly>
-        <SidesSplitter>
-          <Title level={5}>{isLoading ? <>&nbsp;</> : '(Click to open)'}</Title>
+      <SidesSplitter>
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <Hider desktopOnly>
           <Button type="primary" style={{ marginBottom: 24 }}>
             Create new
           </Button>
-        </SidesSplitter>
-      </Hider>
+        </Hider>
+      </SidesSplitter>
 
       {/*Show as empty during loading to reserve space*/}
-      {(isLoading || data?.subscriptions.length) && (
+      {(isLoading || !!filteredSubscriptions.length) && (
         <>
           <Title level={4} mobileOnly>
             {isLoading ? <>&nbsp;</> : '(Press to open)'}
+          </Title>
+          <Title level={5} desktopOnly>
+            {isLoading ? <>&nbsp;</> : '(Click to open)'}
           </Title>
         </>
       )}
@@ -76,15 +88,18 @@ const SubscriptionList: React.FC = () => {
           ))}
       </Row>
 
-      <Title level={3}>Cancelled</Title>
-
-      <Row gutter={[20, 20]}>
-        {cancelledSubscriptions.map((subscription) => (
-          <Col key={subscription.id} xs={24} sm={24} md={12} lg={12}>
-            <Subscription subscription={subscription} />
-          </Col>
-        ))}
-      </Row>
+      {!!cancelledSubscriptions.length && (
+        <>
+          <Title level={3}>Cancelled</Title>
+          <Row gutter={[20, 20]}>
+            {cancelledSubscriptions.map((subscription) => (
+              <Col key={subscription.id} xs={24} sm={24} md={12} lg={12}>
+                <Subscription subscription={subscription} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </MainContentWrapper>
   );
 };
