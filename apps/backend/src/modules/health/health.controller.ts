2e44ad56e2b8e17edb1544e5543d0fc6510884a7
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-02-10 19:21:15 +0600

    Health endpoint

---
+++ apps/backend/src/modules/health/health.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Controller('health')
+export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  @ApiResponse({
+    status: 200,
+    description: 'API server works fine',
+  })
+  @Get('')
+  async health() {
+    const health: { health?: number }[] | undefined = await this.prisma
+      .$queryRaw`SELECT 1+1 as health`;
+
+    if (!health || health[0].health !== 2) {
+      this.logger.error('Health check failed', { health });
+      throw new Error('Health check failed');
+    }
+
+    this.logger.debug('Health check');
+    return;
+  }
+}
